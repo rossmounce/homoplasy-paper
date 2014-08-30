439561ab62765420a85c7d252c1cee0cc1c3ca83
Ross Mounce <ross.mounce@gmail.com>
2014-08-30 22:38:02 +0100

    update

--- r-scripts-and-figures/simple-xy-plots.R
+++ r-scripts-and-figures/simple-xy-plots.R
@@ -1,9 +1,17 @@
+## I'll make pretty plots with ggplot2 later, these are just quick & dirty plots to help visualise the data
+
 setwd("/home/ross/workspace/mygithub/homoplasy-paper")
 megatable <- read.csv("./results-summary/results-in-progress.csv")
 attach(megatable)
 names(megatable)
 
+plot(MHER,CI,main="CI vs MHER", pch=4,xlab="mod. Homoplasy Excess Ratio", ylab="ensemble Consistency Index (CI)",xlim=c(0,1))
+
 plot(RI,CI,main="CI vs RI", pch=4,xlab="ensemble Restriction Index (RI)", ylab="ensemble Consistency Index (CI)")
+
+png('./r-scripts-and-figures/CIvsMHER.png')
+plot(MHER,CI,main="CI vs MHER", pch=4,xlab="mod. Homoplasy Excess Ratio", ylab="ensemble Consistency Index (CI)",xlim=c(0,1))
+dev.off()
 
 png('./r-scripts-and-figures/CIvsRI.png')
 plot(RI,CI,main="CI vs RI", pch=4,xlab="ensemble Restriction Index (RI)", ylab="ensemble Consistency Index (CI)")
