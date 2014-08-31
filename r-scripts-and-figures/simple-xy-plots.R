c8c8428dd53db30cc15b2042257a9325bfd20bef
Ross Mounce <ross.mounce@gmail.com>
2014-08-31 02:11:01 +0100

    labelling outliers on plots

--- r-scripts-and-figures/simple-xy-plots.R
+++ r-scripts-and-figures/simple-xy-plots.R
@@ -4,15 +4,90 @@ setwd("/home/ross/workspace/mygithub/homoplasy-paper")
 megatable <- read.csv("./results-summary/results-in-progress.csv")
 attach(megatable)
 names(megatable)
+zz <- as.data.frame(megatable)
+str(zz)
+names(zz)
+colnames(zz)[5] = "mpts"
+zz[16:22] <- list(NULL)
+names(zz)
+summary(zz)
+cor(zz)                              # correlation matrix
+pairs(zz)                            # scat
+names(zz)                            # for handy reference
+model1 = lm(CI ~ L + MinL + mpts + informativechars + Taxa, data=zz)
+summary(model1)
+summary.aov(model1)
+
+plot(MHER,Taxa,main="Taxa vs MHER", pch=4,xlab="mod. Homoplasy Excess Ratio", ylab="Number of Taxa",xlim=c(0,1),yaxt="n")
+axis(2, at=c(0,20,40,60,80,100,120,140,160,180,200,220),)
+text(0.54,225,"Daza",cex=0.7)
+text(0.8,219,"Longrich",cex=0.7)
+text(0.8,188,"LivezeyZusi07 + Smithea13",cex=0.7)
+text(0.68,154,"Wagner1997a",cex=0.7)
+
+plot(informativechars,Taxa,main="Taxa vs Characters", pch=4,xlab="Number of Informative Characters", ylab="Number of Taxa",xlim=c(0,1100),yaxt="n", col="blue")
+axis(2, at=c(0,20,40,60,80,100,120,140,160,180,200,220),)
+text(290,225,"Daza",cex=0.7)
+text(700,219,"Longrich",cex=0.7)
+text(800,188,"LivezeyZusi07 + Smithea13 [OFFPLOT] -> 2500+",cex=0.7)
+text(220,154,"Wagner1997a",cex=0.7)
+text(940,72,"Naish'12",cex=0.7)
+text(900,101,"Godefroit'13b",cex=0.7)
 
 plot(MHER,CI,main="CI vs MHER", pch=4,xlab="mod. Homoplasy Excess Ratio", ylab="ensemble Consistency Index (CI)",xlim=c(0,1))
-
 plot(RI,CI,main="CI vs RI", pch=4,xlab="ensemble Restriction Index (RI)", ylab="ensemble Consistency Index (CI)")
+
+png('./r-scripts-and-figures/pairs.png')
+pairs(zz)
+dev.off()
 
 png('./r-scripts-and-figures/CIvsMHER.png')
 plot(MHER,CI,main="CI vs MHER", pch=4,xlab="mod. Homoplasy Excess Ratio", ylab="ensemble Consistency Index (CI)",xlim=c(0,1))
 dev.off()
 
+png('./r-scripts-and-figures/RIvsMHER.png')
+plot(MHER,RI,main="RI vs MHER", pch=4,xlab="mod. Homoplasy Excess Ratio", ylab="ensemble Restriction Index (RI)",xlim=c(0,1))
+dev.off()
+
+png('./r-scripts-and-figures/TaxavsMHER.png')
+plot(MHER,Taxa,main="Taxa vs MHER", pch=4,xlab="mod. Homoplasy Excess Ratio", ylab="Number of Taxa",xlim=c(0,1),yaxt="n",col="blue")
+axis(2, at=c(0,20,40,60,80,100,120,140,160,180,200,220),)
+text(0.54,225,"Daza",cex=0.7)
+text(0.8,219,"Longrich",cex=0.7)
+text(0.8,188,"LivezeyZusi07 + Smithea13",cex=0.7)
+text(0.68,154,"Wagner1997a",cex=0.7)
+dev.off()
+
+png('./r-scripts-and-figures/TaxavsCharacters.png')
+plot(informativechars,Taxa,main="Taxa vs Characters", pch=4,xlab="Number of Informative Characters", ylab="Number of Taxa",xlim=c(0,1100),yaxt="n", col="blue")
+axis(2, at=c(0,20,40,60,80,100,120,140,160,180,200,220),)
+text(290,225,"Daza",cex=0.7)
+text(700,219,"Longrich",cex=0.7)
+text(800,188,"LivezeyZusi07 + Smithea13 [OFFPLOT] -> 2500+",cex=0.7)
+text(220,154,"Wagner1997a",cex=0.7)
+text(940,72,"Naish'12",cex=0.7)
+text(900,101,"Godefroit'13b",cex=0.7)
+dev.off()
+
+
+png('./r-scripts-and-figures/TaxavsRI.png')
+plot(RI,Taxa,main="Taxa vs RI", pch=4,xlab="ensemble Restriction Index (RI)", ylab="Number of Taxa",xlim=c(0,1))
+dev.off()
+
 png('./r-scripts-and-figures/CIvsRI.png')
 plot(RI,CI,main="CI vs RI", pch=4,xlab="ensemble Restriction Index (RI)", ylab="ensemble Consistency Index (CI)")
+dev.off()
+
+png('./r-scripts-and-figures/CIvsTaxa.png')
+plot(Taxa,CI,main="CI vs Taxa", pch=4,xlab="Number of Taxa", ylab="ensemble Consistency Index (CI)")
+dev.off()
+
+
+#tutorial http://ww2.coastal.edu/kingw/statistics/R-tutorials/multregr.html
+
+zz[c(1,7,9,10,12,13)] <- list(NULL)
+zz[c(4,8)] <- list(NULL)
+names(zz)
+png('./r-scripts-and-figures/pairs.png')
+pairs(zz)
 dev.off()
