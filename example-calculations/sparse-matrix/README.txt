fa296d2fd2f542c89bf98cae2b5c96a7e89eff97
Ross Mounce <ross.mounce@gmail.com>
2014-10-20 14:08:23 +0100

    added normal HER as well as MHER

--- example-calculations/sparse-matrix/README.txt
+++ example-calculations/sparse-matrix/README.txt
@@ -1,13 +1,16 @@
 To recalculate the statistics for the example sparse matrix figure demonstrating the difference between HER & MHER, run:
 
-bash get-mher.sh sparse_matrix.tnt
+bash get-mher.sh sparse_matrix.tnt #to get MHER
+bash get-her.sh sparse_matrix.tnt #to get HER
 
-get-mher.sh is a bash wrapper script that gives TNT input instructions and parses TNT output to calculate & save the descriptive statistics produced.
+'get-mher.sh' and 'get-her.sh' are bash wrapper scripts that give TNT input instructions whilst also parsing TNT output to calculate & save the descriptive statistics produced.
 
-sparse_matrix.tnt is the hypothetical sparse matrix given in the paper, in TNT format.
+'sparse_matrix.tnt' is the hypothetical sparse matrix given in the paper, in TNT format.
 
-1000reps.txt is essentially a parameter file, in which it is hardcoded to perform 1000 matrix permutation replications.
+'mher.run' is the TNT script which actually performs the selective matrix permutations - only permuting known states.
 
-mher.run is the TNT script which actually performs the selective matrix permutations - only permuting known states.
+'100reps.txt' is essentially a parameter file, in which it is hardcoded to perform 100 selective (MHER) matrix permutation replications.
+
+'her-reps.txt' is another parameter file, in which is hardcoded 100 (HER) matrix permutation replications.
 
 
--- example-calculations/sparse-matrix/get-her.sh
+++ example-calculations/sparse-matrix/get-her.sh
@@ -2,7 +2,7 @@
 
 #This is a bash script to perform the modified Homoplasy Excess Ratio
 #on a dataset of your choice passed to this script as an argument
-#You also need the files 'mher.run' AND '100reps.txt' in the same dir
+#You also need the files 'mher.run' AND 'reps.txt' in the same dir
 
 #The MIT License (MIT)
 
@@ -18,14 +18,14 @@
 #DATA FILE TO BE TESTED MUST END with 'procedure /;'
 #this makes a copy of the data file with 
 #additional instructions appended to tmp.tnt
-sed 's@procedure \/;@log temp1.log; minmax\*; sect:slack 40; xmult=level10; log\/; log output_reps.log; mher start; proc 100reps.txt; quit;@' $1 > tmp.tnt
+sed 's@procedure \/;@log temp1.log; minmax\*; sect:slack 40; xmult=level10; log\/; log her_output_reps.log; proc her_reps.txt; quit;@' $1 > tmp.tnt
 
 #ensure that all inapplicables have been converted to ? marks
 sed -i 's/-/?/g' tmp.tnt;
 #put back hyphens in ccode block if they were taken out by the above command
 sed -i 's/?\[\//-\[\//g' tmp.tnt;
 
-#this will do everything then quit. output hardcoded to output.log
+#this will do everything then quit. output hardcoded to output_reps.log
 tnt proc tmp.tnt; 
 
 mkdir output-${1%.tnt}
@@ -33,7 +33,7 @@ mkdir output-${1%.tnt}
 printf "MEANNS calculations in TNT are complete \n"
 
 #hacky shorter way of gettings the modified-MEANNS
-grep 'Best score:' output_reps.log | awk '{sum+=$3} END { print sum/NR}' > ./output-${1%.tnt}/mns.tmp
+grep 'Best score:' her_output_reps.log | awk '{sum+=$3} END { print sum/NR}' > ./output-${1%.tnt}/her-mns.tmp
 
 #get MINL, temp1 is the first logfile output from TNT
 head -1 temp1.log | sed 's/\// /g' | cut -d ' ' -f 8 > ./output-${1%.tnt}/minl.tmp
@@ -41,13 +41,14 @@ head -1 temp1.log | sed 's/\// /g' | cut -d ' ' -f 8 > ./output-${1%.tnt}/minl.t
 tail -1 temp1.log | sed 's/\./ /g' | cut -d ' ' -f 3 > ./output-${1%.tnt}/l.tmp
 
 #print results
-printf "MINL = `cat ./output-${1%.tnt}/minl.tmp` \n"
-printf "L = `cat ./output-${1%.tnt}/l.tmp` \n"
-printf "Modified-MEANNS = `cat ./output-${1%.tnt}/mns.tmp` \n"
-paste ./output-${1%.tnt}/mns.tmp ./output-${1%.tnt}/minl.tmp ./output-${1%.tnt}/l.tmp | awk '{o = ($1-$3)/($1-$2)} END { print "Modified-HER = " o }' | tee ./output-${1%.tnt}/MHER.out
+printf "MINL = `cat minl.tmp` \n" 
+printf "L = `cat l.tmp` \n" 
+printf "Modified-MEANNS = `cat her-mns.tmp` \n" 
+paste ./output-${1%.tnt}/her-mns.tmp ./output-${1%.tnt}/minl.tmp ./output-${1%.tnt}/l.tmp | awk '{o = ($1-$3)/($1-$2)} END { print "Homplasy Excess Ratio = " o }' | tee ./output-${1%.tnt}/normal_her_result.txt
 
 #clean up temporary files but leave behind permuted matrix log file
 rm temp1.log;
 rm tmp.tnt; 
-rm output_reps.log; 
+rm her_output_reps.log; 
+ 
 
